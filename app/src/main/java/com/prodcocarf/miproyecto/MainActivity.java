4ade8c109e8f29ae014a9fb309f09c46eefe3c89
Fernando <fercho_17cal@hotmail.com>
2023-05-19 18:06:10 -0500

    dos actividades

--- app/src/main/java/com/prodcocarf/miproyecto/MainActivity.java
+++ app/src/main/java/com/prodcocarf/miproyecto/MainActivity.java
@@ -2,13 +2,40 @@ package com.prodcocarf.miproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button irRegistrarUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	//ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_usuarios",null,1);
+        irRegistrarUsuario=(Button)findViewById(R.id.btnOpcionRegistro);
+        irRegistrarUsuario.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v)
+    {
+
+        	Intent miIntent=null;
+
+        	//switch (view.getId()){
+               // case R.id.btnOpcionRegistro:
+                	miIntent=new Intent(MainActivity.this,RegistroUsuariosActivity.class);
+                	//break;
+
+       // }
+       // if (miIntent!=null){
+        //    startActivity(miIntent);
+       // }
+
     }
 }
